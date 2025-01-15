bb4966a042dc9108ab23078962a06664bd20c391
tsilmak <tsilmak22@gmail.com>
2025-01-15 19:56:52 +0000

    seed alter

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -188,7 +188,7 @@ async function main() {
         birthDate: "1999-01-09T00:00:00Z",
         docType: "bi",
         docNumber: "12233123123",
-        nif: "PT1224243123123",
+        nif: "PT124424243123123",
         address: "Rua dos Gatos",
         zipcode: "1000-100",
         country: "pt",
@@ -279,125 +279,6 @@ await prisma.exercise.createMany({
       imagePublicId: "v0fgwt5vkzfhiz99jh6c",
       equipmentId: MaquinaRemo.MachineId,
     },
-  ],
-});
-
-await prisma.blogCategory.createMany({
-  data: [
-    {
-      categoryId: 1,
-      name: "Treinos e Exercícios",
-    },
-    {
-      categoryId: 2,
-      name: "Nutrição e Dieta",
-    },
-    {
-      categoryId: 3,
-      name: "Saúde e Bem-estar",
-    },
-    {
-      categoryId: 4,
-      name: "Tecnologia e Inovação no Fitness",
-    },
-    {
-      categoryId: 5,
-      name: "Histórias de Sucesso e Motivação",
-    },
-    {
-      categoryId: 6,
-      name: "Gestão de Ginásios",
-    },
-    {
-      categoryId: 7,
-      name: "Treinamento Funcional",
-    },
-    {
-      categoryId: 8,
-      name: "Fitness para Idosos",
-    },
-    {
-      categoryId: 9,
-      name: "Mindfulness e Yoga",
-    },
-    {
-      categoryId: 10,
-      name: "Equipamentos de Ginásio",
-    },
-    {
-      categoryId: 11,
-      name: "Desafios e Competências Fitness",
-    },
-    {
-      categoryId: 12,
-      name: "Moda Fitness e Roupas para Treino",
-    },
-    {
-      categoryId: 13,
-      name: "Treinamento Mental e Psicologia do Esporte",
-    },
-  ],
-});
-
-await prisma.blog.createMany({
-  data: [
-    {
-      blogId: "31a51d3d-7e08-41b6-9fbc-4a131120e495",
-      title:
-        "A Importância da Nutrição Balanceada: Como Escolher a Dieta Certa para o Seu Corpo",
-      body: `
-        <p>A alimentação é um dos pilares essenciais para a manutenção de uma vida saudável e equilibrada. Uma nutrição adequada não apenas promove a saúde física, mas também impacta diretamente o nosso bem-estar emocional e mental. No entanto, com tantas dietas e modismos alimentares a surgirem a cada dia, pode ser difícil saber qual é a escolha certa para o seu corpo e estilo de vida.</p>
-        <p>Neste artigo, vamos explorar a importância de uma dieta balanceada, desmistificar os diferentes tipos de planos alimentares e dar-lhe as ferramentas necessárias para tomar decisões mais informadas sobre a sua nutrição.</p>
-        <h3><strong>O que é uma Nutrição Balanceada?</strong></h3>
-        <p>Nutrição balanceada significa consumir uma variedade de alimentos que forneçam ao corpo os nutrientes essenciais para o seu funcionamento adequado. Estes nutrientes incluem proteínas, carboidratos, gorduras saudáveis, vitaminas e minerais. Quando o corpo recebe os nutrientes certos em quantidades apropriadas, é capaz de funcionar de forma mais eficiente, melhorar a energia e reduzir o risco de doenças crónicas.</p>
-        <h4><strong>Principais Nutrientes e as Suas Funções:</strong></h4>
-        <ol>
-          <li><strong>Proteínas</strong>: Essenciais para o crescimento muscular, reparação de tecidos e funções do sistema imunológico.</li>
-          <li><strong>Carboidratos</strong>: Fonte principal de energia, especialmente para a função cerebral e atividades físicas.</li>
-          <li><strong>Gorduras saudáveis</strong>: Necessárias para o funcionamento do sistema nervoso e a absorção de vitaminas lipossolúveis.</li>
-          <li><strong>Vitaminas e Minerais</strong>: Cruciais para uma variedade de processos no corpo, incluindo a regulação do metabolismo e o apoio ao sistema imunológico.</li>
-        </ol>
-        <h3><strong>Diferentes Tipos de Dietas: Qual é a Melhor para Si?</strong></h3>
-        <p>Com a popularidade de dietas específicas como a cetogénica, vegana ou paleo, pode ser tentador seguir a última tendência sem considerar se ela é adequada ao seu corpo ou objetivos. É importante entender que cada pessoa tem necessidades nutricionais diferentes, e o que funciona para uma pessoa pode não ser ideal para outra.</p>
-        <p>Aqui estão algumas dietas populares e os seus benefícios:</p>
-        <h4><strong>1. Dieta Mediterrânica</strong></h4>
-        <p>Focada em alimentos frescos e naturais, como frutas, vegetais, peixes, azeite e grãos integrais, esta dieta é reconhecida pelos seus benefícios para a saúde cardiovascular e longevidade.</p>
-        <h4><strong>2. Dieta Cetogénica</strong></h4>
-        <p>Composta principalmente por gorduras saudáveis, proteínas e uma quantidade muito reduzida de carboidratos, a dieta cetogénica visa colocar o corpo em estado de cetose, o que pode ajudar na perda de peso.</p>
-        <h4><strong>3. Dieta Vegana</strong></h4>
-        <p>Exclui todos os produtos de origem animal, focando-se em vegetais, leguminosas, nozes e sementes. É rica em fibras e antioxidantes, mas exige planejamento para garantir que todos os nutrientes sejam obtidos.</p>
-        <h4><strong>4. Dieta Paleo</strong></h4>
-        <p>Baseada no consumo de alimentos que nossos ancestrais caçadores-recolectores comiam, a dieta paleo evita alimentos processados, laticínios, grãos e leguminosas, priorizando carnes magras, peixes, frutas e vegetais.</p>
-        <h3><strong>Como Escolher a Dieta Certa para Si?</strong></h3>
-        <p>A escolha da dieta ideal deve ser personalizada e baseada em vários fatores, incluindo os seus objetivos de saúde, condições médicas, nível de atividade física e preferências alimentares. Consultar um nutricionista é sempre uma excelente opção, pois ele pode criar um plano alimentar que atenda às suas necessidades específicas.</p>
-        <ol>
-          <li><strong>Defina os seus objetivos</strong>: Quer perder peso, melhorar a saúde cardiovascular ou aumentar a massa muscular? Isso pode influenciar a sua escolha alimentar.</li>
-          <li><strong>Considere o seu estilo de vida</strong>: Se é uma pessoa com uma agenda ocupada, opte por uma dieta prática e fácil de seguir.</li>
-          <li><strong>Evite modismos</strong>: Lembre-se de que uma dieta equilibrada é mais eficaz e sustentável do que seguir as últimas tendências.</li>
-        </ol>
-        <h3><strong>A Importância da Hidratação e do Equilíbrio</strong></h3>
-        <p>Além de uma alimentação saudável, a hidratação desempenha um papel crucial na manutenção do bem-estar. A água é fundamental para a digestão, transporte de nutrientes e regulação da temperatura corporal. Manter-se hidratado é uma prática simples, mas muitas vezes negligenciada.</p>
-        <p>Por fim, lembre-se de que a chave para uma vida saudável não está em restrições extremas, mas sim em equilíbrio. A nutrição é uma jornada contínua que envolve escolhas alimentares conscientes, prática regular de exercício físico e um bom descanso.</p>
-        <h3><strong>Conclusão</strong></h3>
-        <p>Nutrição e dieta são mais do que simples escolhas alimentares; são a base para uma vida longa e saudável. Ao compreender as suas necessidades nutricionais e adotar uma dieta equilibrada, pode melhorar a sua saúde geral e aumentar a qualidade de vida. Lembre-se de que cada corpo é único e merece um plano alimentar personalizado. E, acima de tudo, faça sempre escolhas que sejam sustentáveis a longo prazo, para que a saúde não seja apenas um objetivo, mas um estilo de vida.</p>
-      `,
-      coverImageUrl:
-        "https://res.cloudinary.com/dmfbmt6mi/image/upload/v1736715275/eb2u1rxxf14wybghjx4q.jpg",
-      coverImagePublicId: "eb2u1rxxf14wybghjx4q",
-      published: true,
-      createdAt: new Date("2025-01-12T20:54:36.447Z"),
-      updatedAt: new Date("2025-01-12T20:54:36.447Z"),
-    },
-  ],
-});
-
-await prisma.blogAuthor.createMany({
-  data: [
-    {
-      blogId: "31a51d3d-7e08-41b6-9fbc-4a131120e495",
-      userId: 1,
-    },
-    { blogId: "31a51d3d-7e08-41b6-9fbc-4a131120e495", userId: 2 },
   ],
 });
 
