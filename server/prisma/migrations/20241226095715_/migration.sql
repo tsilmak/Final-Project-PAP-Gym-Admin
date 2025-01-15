2bf92815fbc9db12173161a37c92efdda2935ba1
tsilmak <tsilmak22@gmail.com>
2025-01-15 18:59:54 +0000

    remove migration

--- server/prisma/migrations/20241226095715_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "WorkoutCompletion" (
-    "workoutCompletionId" TEXT NOT NULL,
-    "userWhoCompleted" INTEGER NOT NULL,
-    "workoutPlanSnapshot" JSONB NOT NULL,
-    "summary" JSONB NOT NULL,
-
-    CONSTRAINT "WorkoutCompletion_pkey" PRIMARY KEY ("workoutCompletionId")
-);
